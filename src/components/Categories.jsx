f7e711e7ad66d63201ed7843a7efe388c22d09b6
ruslan <rnagumanov57@gmail.com>
2024-03-22 16:32:23 +0300

    pagination

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,36 +1,19 @@
-import React from "react";
+import React from 'react';
 
-const Categories = () => {
-  const [activeCategory, setActiveCategory] = React.useState(0);
+const Categories = ({ value, onChangeCategory }) => {
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
-  const categories = [
-    "Все",
-    "Мясные",
-    "Вегетарианская",
-    "Гриль",
-    "Острые",
-    "Закрытые",
-  ];
-
-  const onClickCategory = (index) => {
-    setActiveCategory(index);
-  };
-
-  return (
-    <div className="categories">
-      <ul>
-        {categories.map((value, index) => (
-          <li
-            key={index}
-            onClick={() => onClickCategory(index)}
-            className={activeCategory === index ? "active" : ""}
-          >
-            {value}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((categoryName, index) => (
+                    <li key={index} onClick={() => onChangeCategory(index)} className={value === index ? 'active' : ''}>
+                        {categoryName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,47 @@
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import Skeleton from "../components/PizzaBlock/Skeleton";
-import PizzaBlock from "../components/PizzaBlock/index";
-import React from "react";
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import PizzaBlock from '../components/PizzaBlock/index';
+import React from 'react';
 
-const Home = () => {
-  const [pizzas, setPizzas] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
+const Home = ({ searchValue }) => {
+    const [pizzas, setPizzas] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [categoryId, setCategoryId] = React.useState(0);
+    const [sortType, setSortType] = React.useState({
+        name: 'популярности',
+        sortProperty: 'rating',
+    });
 
-  React.useEffect(() => {
-    fetch("https://655a10ee6981238d054d1578.mockapi.io/pizzas")
-      .then((res) => res.json())
-      .then((arr) => {
-        setPizzas(arr);
-        setIsLoading(false);
-      });
-  }, []);
+    React.useEffect(() => {
+        setIsLoading(true);
 
-  return (
-    <div className="container">
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-      </div>
-    </div>
-  );
+        const sortBy = sortType.sortProperty.replace('-', '');
+        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+        fetch(`https://655a10ee6981238d054d1578.mockapi.io/pizzas?${category}&sortBy=${sortBy}&order=${order}`)
+            .then(res => res.json())
+            .then(arr => {
+                setPizzas(arr);
+                setIsLoading(false);
+            });
+        window.scrollTo(0, 0);
+    }, [categoryId, sortType]);
+
+    const items = pizzas.filter(pizza => pizza.title.toLowerCase().includes(searchValue.toLowerCase())).map(pizza => <PizzaBlock key={pizza.id} {...pizza} />);
+    const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+    return (
+        <div className="container">
+            <div className="content__top">
+                <Categories value={categoryId} onChangeCategory={index => setCategoryId(index)} />
+                <Sort value={sortType} onChangeSort={index => setSortType(index)} />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">{isLoading ? skeletons : items}</div>
+        </div>
+    );
 };
 
 export default Home;
